0970fcddf02295ca7e57cfd432cbce85152b3f33
Monu Singh <125896049+monurajputrko@users.noreply.github.com>
2024-03-19 00:59:58 +0530

    Files Added

---
+++ backend/app.js
@@ -0,0 +1,50 @@
+import express from "express";
+import { dbConnection } from "./database/dbConnection.js";
+import { config } from "dotenv";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import fileUpload from "express-fileupload";
+import { errorMiddleware } from "./middlewares/error.js";
+import messageRouter from "./router/messageRouter.js";
+import userRouter from "./router/userRouter.js";
+import appointmentRouter from "./router/appointmentRouter.js";
+
+const app = express();
+config({ path: "./config.env" });
+
+const allowedOrigins = [
+  "https://hospital-managment-sandy.vercel.app",
+  "https://hospital-managment-admin.vercel.app",
+];
+
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+    credentials: true, // Enable credentials (cookies)
+  })
+);
+
+app.use(cookieParser());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(
+  fileUpload({
+    useTempFiles: true,
+    tempFileDir: "/tmp/",
+  })
+);
+app.use("/api/v1/message", messageRouter);
+app.use("/api/v1/user", userRouter);
+app.use("/api/v1/appointment", appointmentRouter);
+
+dbConnection();
+
+app.use(errorMiddleware);
+export default app;
---
+++ dashboard/src/components/Doctors.jsx
@@ -0,0 +1,67 @@
+import axios from "axios";
+import { useContext, useEffect, useState } from "react";
+import { toast } from "react-toastify";
+import { Context } from "../main";
+import { Navigate } from "react-router-dom";
+
+const Doctors = () => {
+  const [doctors, setDoctors] = useState([]);
+  const { isAuthenticated } = useContext(Context);
+  useEffect(() => {
+    const fetchDoctors = async () => {
+      try {
+        const { data } = await axios.get(
+          "https://hospital-managment-api.vercel.app/api/v1/user/doctors",
+          { withCredentials: true }
+        );
+        setDoctors(data.doctors);
+      } catch (error) {
+        toast.error(error.response.data.message);
+      }
+    };
+    fetchDoctors();
+  }, []);
+
+  if (!isAuthenticated) {
+    return <Navigate to={"/login"} />;
+  }
+  return (
+    <section className="page doctors">
+      <h1>DOCTORS</h1>
+      <div className="banner">
+        {doctors && doctors.length > 0 ? (
+          doctors.map((element,i) => {
+            return (
+              <div key={i} className="card">
+                <img src="http://localhost:5174/doc.png" alt="doctor avatar" />
+                <h4>{`${element.firstName} ${element.lastName}`}</h4>
+                <div className="details">
+                  <p>
+                    Email: <span>{element.email}</span>
+                  </p>
+                  <p>
+                    Phone: <span>{element.phone}</span>
+                  </p>
+                  <p>
+                    DOB: <span>{element.dob.substring(0, 10)}</span>
+                  </p>
+                  <p>
+                    Department: <span>{element.doctorDepartment}</span>
+                  </p>
+
+                  <p>
+                    Gender: <span>{element.gender}</span>
+                  </p>
+                </div>
+              </div>
+            );
+          })
+        ) : (
+          <h1>No Registered Doctors Found!</h1>
+        )}
+      </div>
+    </section>
+  );
+};
+
+export default Doctors;
